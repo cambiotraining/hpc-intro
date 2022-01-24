c59190fd8df4ae4df1b01e86e97197d74d6223b5
Hugo Tavares <hm533@cam.ac.uk>
2022-01-24 16:29:49 +0000

    update exercise page

--- compile_exercises.sh
+++ compile_exercises.sh
@@ -2,4 +2,8 @@
 
 # ===== Compile Exercises ===== #
 
-cat *.md | sed -n '/^:::exercise/,/^:::/p' | sed 's/:::exercise/\n----\n:::exercise/g' > 99-exercises.md
+echo ":::note" > 99-exercises.md
+echo "This page contains all the same exercises found throughout the materials. They are compiled here for convenience." >> 99-exercises.md
+echo ":::" >> 99-exercises.md
+
+cat *.md | sed -n '/^:::exercise/,/^:::/p' | sed 's/:::exercise/\n----\n\n:::exercise/g' >> 99-exercises.md
