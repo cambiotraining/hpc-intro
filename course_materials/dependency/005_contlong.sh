cafcdb331c3fc9ba94648c3c2ebe0be2644d48ea
Hugo Tavares <hm533@cam.ac.uk>
2022-04-22 10:00:38 +0100

    fix job dependency scripts

--- course_materials/dependency/005_contlong.sh
+++ course_materials/dependency/005_contlong.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH -p training  # name of the partition to run job on
-#SBATCH -D /home/participant58/dep_test
 #SBATCH -o dep2.log
 #SBATCH -c 1        # number of CPUs. Default: 1
 #SBATCH --mem=1G    # RAM memory. Default: 1G
