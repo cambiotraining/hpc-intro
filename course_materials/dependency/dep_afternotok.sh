5e27c4e968728c6a326278d2acdb04fddfb010bc
Hugo Tavares <hm533@cam.ac.uk>
2023-07-11 11:36:23 +0100

    fix bug

--- course_materials/dependency/dep_afternotok.sh
+++ course_materials/dependency/dep_afternotok.sh
@@ -3,4 +3,4 @@ rm *.txt
 RUN_ID_1=$(sbatch --parsable 004_startlong.sh)
 echo "First job submitted with the run ID ${RUN_ID_1}"
 
-sbatch --dependency=afterok:${RUN_ID_1} 005_contlong.sh
+sbatch --dependency=afternotok:${RUN_ID_1} 005_contlong.sh
