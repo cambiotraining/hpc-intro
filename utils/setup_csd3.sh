78d87fd81e5e562eedfb52cb99cc2c2b6c124737
Hugo Tavares <hm533@cam.ac.uk>
2026-05-26 14:35:42 +0100

    update setup script

--- utils/setup_csd3.sh
+++ utils/setup_csd3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # backup bashrc
 cp ~/.bashrc ~/.bashrc_bkp
 
@@ -7,17 +9,19 @@ cp ~/.bashrc ~/.bashrc_bkp
 unzip ~/rds/rds-introhpc/data.zip -d ~/rds/hpc-work/hpc_workshop
 
 # install mamba
-bash ~/rds/rds-introhpc/Miniforge3-$(uname)-$(uname -m).sh -b -p $HOME/miniforge3
-$HOME/miniforge3/bin/mamba shell init --shell bash
-source ~/.bashrc
+MAMBADIR="$HOME/rds/hpc-work/miniforge3"
+bash ~/rds/rds-introhpc/Miniforge3-$(uname)-$(uname -m).sh -b -p $MAMBADIR
+
+# set up
+. "$MAMBADIR/etc/profile.d/conda.sh"
+. "$MAMBADIR/etc/profile.d/mamba.sh"
 conda config --add channels bioconda; conda config --add channels conda-forge
 conda config --set remote_read_timeout_secs 1000
 
-# set R up
-mamba install -y -n base r-argparser==0.7.3
-
 # additional conda environments
-mamba create -y -n scipy scipy==1.12.0 numpy==1.26.4 matplotlib==3.8.3
+mamba create -y -n np numpy==1.26.4 matplotlib==3.8.3
 mamba create -y -n mapping bowtie2==2.5.3
 
-echo "mamba activate base" >> ~/.bashrc
+# configure bashrc to load conda and mamba
+echo '. "$HOME/rds/hpc-work/miniforge3/etc/profile.d/conda.sh"' >> ~/.bashrc
+echo '. "$HOME/rds/hpc-work/miniforge3/etc/profile.d/mamba.sh"' >> ~/.bashrc
